64c37b2ae23daf5027d62d97dffe9999d9e3eace
dev-vming <dev.vming@gmail.com>
2025-02-27 22:02:40 +0900

    feat: 프로필 및 게시글 상세 컴포넌트 작업

---
+++ src/components/PostDetail.tsx
@@ -0,0 +1,31 @@
+export default function PostDetail() {
+  return (
+    <>
+      <div className="post__detail">
+        <div className="post__box">
+          <div className="post__title">
+            The standard Lorem Ipsum passage, used since the 1500s
+          </div>
+          <div className="post__profile-box">
+            <div className="post__profile" />
+            <div className="post__author-name">사용자</div>
+            <div className="post__date">2025.02.27 목요일</div>
+          </div>
+          <div className="post__utils-box">
+            <div className="post__delete">삭제</div>
+            <div className="post__edit">수정</div>
+          </div>
+          <div className="post__text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,20 +1,20 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Home from "../pages/home";
-import PostList from "../pages/posts";
-import PostDetail from "../pages/posts/detail";
 import PostNew from "../pages/posts/new";
 import PostEdit from "../pages/posts/edit";
 import ProfilePage from "../pages/profile";
 import LoginPage from "../pages/login";
 import SignupPage from "../pages/signup";
+import PostPage from "../pages/posts/detail";
+import PostsPage from "../pages/posts";
 
 export default function Router() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<PostList />} />
-        <Route path="/posts/:id" element={<PostDetail />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<PostPage />} />
         <Route path="/posts/new" element={<PostNew />} />
         <Route path="/posts/edit/:id" element={<PostEdit />} />
         <Route path="/profile" element={<ProfilePage />} />
