9daa407132bdf05b7baa8ec536a34b958638dbba
dev-vming <dev.vming@gmail.com>
2025-02-28 22:11:57 +0900

    feat: 회원가입 작업

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import Router from "./components/Router";
 import { useState } from "react";
 import { app } from "firebaseApp";
 import { getAuth } from "firebase/auth";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   const auth = getAuth(app);
-  console.log(auth)
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
-  return <Router isAuthenticated={isAuthenticated} />;
-}
+  return (
+    <>
+      <ToastContainer />
+      <Router isAuthenticated={isAuthenticated} />
+    </>);
+};
 
 export default App;
