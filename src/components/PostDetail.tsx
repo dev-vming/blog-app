bcc0984133f15b08860a8d0c0755280fea0e74ea
dev-vming <dev.vming@gmail.com>
2025-03-08 00:06:14 +0900

    feat: 게시물 삭제 deleteDoc 작업

--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -1,13 +1,15 @@
-import { doc, getDoc } from "firebase/firestore";
+import { deleteDoc, doc, getDoc } from "firebase/firestore";
 import { db } from "firebaseApp";
 import { useContext, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { PostProps } from "./PostList";
 import Loader from "./Loader";
 import AuthContext from "context/AuthContext";
+import { toast } from "react-toastify";
 
 export default function PostDetail() {
   const params = useParams();
+  const navigate = useNavigate();
   const { user } = useContext(AuthContext);
   const [post, setPost] = useState<PostProps | null>(null);
 
@@ -19,8 +21,13 @@ export default function PostDetail() {
     }
   };
 
-  const handleDelete = () => {
-    console.log("delete!");
+  const handleDelete = async () => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && post && post.id) {
+      await deleteDoc(doc(db, "posts", post.id));
+      toast?.success("게시글을 삭제했습니다.");
+      navigate("/");
+    }
   };
 
   useEffect(() => {
--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,8 +1,9 @@
 import { Link } from "react-router-dom";
 import { useContext, useEffect, useState } from "react";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs } from "firebase/firestore";
 import { db } from "firebaseApp";
 import AuthContext from "context/AuthContext";
+import { toast } from "react-toastify";
 
 interface PostListProps {
   hasNavigation?: boolean;
@@ -28,11 +29,20 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
 
   const getPosts = async () => {
     const datas = await getDocs(collection(db, "posts"));
-
+    setPosts([]);
     datas?.forEach((doc) => {
       const dataObj = { ...doc.data(), id: doc.id };
       setPosts((prev) => [...prev, dataObj as PostProps]);
     });
+  };
+
+  const handleDelete = async (id: string) => {
+    const confirm = window.confirm("해당 게시글을 삭제하시겠습니까?");
+    if (confirm && id) {
+      await deleteDoc(doc(db, "posts", id));
+      toast.success("게시글을 삭제했습니다.");
+      getPosts(); // 변경된 포스트 리스트를 다시 가져옴
+    }
   };
 
   useEffect(() => {
@@ -72,7 +82,12 @@ export default function PostList({ hasNavigation = true }: PostListProps) {
               </Link>
               {post?.email === user?.email && (
                 <div className="post__utils-box">
-                  <div className="post__delete">삭제</div>
+                  <div
+                    className="post__delete"
+                    onClick={() => handleDelete(post.id as string)}
+                  >
+                    삭제
+                  </div>
                   <div className="post__edit">
                     <Link to={`/posts/edit/${post?.id}`}>수정</Link>
                   </div>
