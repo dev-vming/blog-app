b8f5add4726ddc52be86506bddc0a2025d337e4e
dev-vming <dev.vming@gmail.com>
2025-03-07 23:41:38 +0900

    feat: 수정, 제출 버튼 분기 처리

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -121,7 +121,7 @@ export default function PostForm() {
         />
       </div>
       <div className="form__block">
-        <input type="submit" value="제출" className="form__btn--submit" />
+        <input type="submit" value={post ? "수정" : "제출"} className="form__btn--submit" />
       </div>
     </form>
   );
