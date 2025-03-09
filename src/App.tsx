a2aac670f53afbad430c1e65f9aa6b389f4b22ce
dev-vming <dev.vming@gmail.com>
2025-03-09 21:01:38 +0900

    feat: 다크모드 css 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import Router from "./components/Router";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { app } from "firebaseApp";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import Loader from "components/Loader";
+import ThemeContext from "context/ThemeContext";
 
 function App() {
+  const context = useContext(ThemeContext);
   const auth = getAuth(app);
-  // auth를 체크하기 전에 (initialize 전) loader를 띄우는 용도
   const [init, setInit] = useState<boolean>(false);
-  // auth의 currentUser가 있으면 authenticated로 변경
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(!!auth?.currentUser);
 
   useEffect(() => {
@@ -25,10 +25,10 @@ function App() {
   }, [auth]);
 
   return (
-    <>
+    <div className={context.theme === "light" ? "light" : "dark"}>
       <ToastContainer />
       {init ? <Router isAuthenticated={isAuthenticated} /> : <Loader />}
-    </>);
+    </div>);
 };
 
 export default App;
