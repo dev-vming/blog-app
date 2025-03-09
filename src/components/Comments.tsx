6ef293dfa894e543b867df71468b1f9117a174ef
dev-vming <dev.vming@gmail.com>
2025-03-09 22:44:30 +0900

    feat: 댓글 기능 작업

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,57 +1,17 @@
-import { useState } from "react";
+import React, { useContext, useState } from "react";
+import { PostProps } from "./PostList";
+import { arrayUnion, doc, updateDoc } from "firebase/firestore";
+import { db } from "firebaseApp";
+import AuthContext from "context/AuthContext";
+import { toast } from "react-toastify";
 
-const COMMENTS = [
-  {
-    id: 1,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 2,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 3,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 4,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 5,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 6,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 7,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-  {
-    id: 8,
-    email: "test@test.com",
-    content: "댓글입니다.",
-    createdAt: "2023-07-13"
-  },
-]
+interface CommentProps {
+  post: PostProps;
+  getPost: (id:string) => Promise<void>;
+}
 
-export default function Comments() {
+export default function Comments({ post, getPost }: CommentProps) {
+  const { user } = useContext(AuthContext);
   const [comment, setComment] = useState<string>("");
   const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     const {
@@ -62,9 +22,43 @@ export default function Comments() {
       setComment(value);
     }
   };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (post && post?.id) {
+        const postRef = doc(db, "posts", post.id);
+        if (user?.uid) {
+          const commentObj = {
+            content: comment,
+            uid: user.uid,
+            email: user.email,
+            createdAt: new Date()?.toLocaleDateString("ko", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }),
+          };
+
+          await updateDoc(postRef, {
+            comments: arrayUnion(commentObj),
+            updatedAt: new Date()?.toLocaleDateString("ko", {
+              hour: "2-digit",
+              minute: "2-digit",
+              second: "2-digit",
+            }),
+          });
+          await getPost(post.id);
+        }
+      }
+      toast.success("댓글을 생성했습니다.");
+      setComment("");
+    } catch (e: any) {
+      toast.error(e?.code);
+    }
+  };
   return (
     <div className="comments">
-      <form className="comments__form">
+      <form className="comments__form" onSubmit={onSubmit}>
         <div className="form__block">
           <label htmlFor="comment">댓글 입력</label>
           <textarea
@@ -76,16 +70,12 @@ export default function Comments() {
           />
         </div>
         <div className="form__block form__block-reverse">
-        <input
-          type="submit"
-          value="입력"
-          className="form__btn--submit"
-        />
-      </div>
+          <input type="submit" value="입력" className="form__btn--submit" />
+        </div>
       </form>
       <div className="comments__list">
-        {COMMENTS?.map((comment) => (
-          <div key={comment.id} className="comment__box">
+        {post?.comments?.slice(0).reverse().map((comment) => (
+          <div key={comment?.createdAt} className="comment__box">
             <div className="comment__profile-box">
               <div className="comment__email">{comment?.email}</div>
               <div className="comment__date">{comment?.createdAt}</div>
--- src/components/PostDetail.tsx
+++ src/components/PostDetail.tsx
@@ -63,7 +63,7 @@ export default function PostDetail() {
               {post?.content}
             </div>
           </div>
-          <Comments />
+          <Comments post={post} getPost={getPost} />
         </>
       ) : (
         <Loader />
